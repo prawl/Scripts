32c0568f94d609de203e9f956098bf3b84503c1b
Patrick Rawlings <ptyrawlings@gmail.com>
2014-05-20 00:51:25 -0400

    Scrabble solver now comparing words

--- scrabble_solver.rb
+++ scrabble_solver.rb
@@ -5,7 +5,7 @@ def build_words
   @words = []
   File.open('/usr/share/dict/words') do |file|
     file.each do |line|
-     @words << line
+      @words << line.chomp
     end
   end
 end
@@ -13,24 +13,22 @@ end
 def anagram_builder(input)
   #story each letter of string into an array
   word = input.split(%r{\s*})
-  val = word.repeated_permutation(word.size).to_a
-  arr = []
-  val.each do |t|
-   val = t.join''
-  end
-  puts val.size
+  @val = word.repeated_permutation(word.size).to_a
+  @val.map! { |t| t.join"" }
+  @val.uniq!
 end
 
 def word_checker
-  puts "Enter a word and I'll check if it's in the dictionary"
 
   while true
+    puts "Enter a word and I'll check if it's in the dictionary"
     input = gets.downcase
     anagram_builder(input)
-    if @words.include? input
-      puts "Valid word"
-    else
-      puts "Invalid word"
+     #  puts "Comparing {#@words} and {#i}"
+      @val.each do |x|
+        if @words.include? x.chomp
+        puts " #{x} is a valid word"
+      end
     end
   end
 end
